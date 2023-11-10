1c43129a9584e2fae10eac20d8fe2e98995d4a78
lonelam <laizenan@gmail.com>
2023-11-11 06:08:30 +0800

    fix marked katex problem

--- generate.js
+++ generate.js
@@ -3,7 +3,10 @@ const markedKatex = require("marked-katex-extension");
 async function main() {
   const hexo = new Hexo(process.cwd(), {});
   hexo.extend.filter.register("marked:extensions", function (extensions) {
-    extensions.push(markedKatex({}));
+    const addedExtentions = markedKatex({
+      throwOnError: false,
+    });
+    extensions.push(...addedExtentions.extensions);
   });
   await hexo.init();
   await hexo.call("generate", {});
